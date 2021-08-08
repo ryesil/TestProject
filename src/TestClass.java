81e29dddd098f4ef65c80f2c820acaa638eeeffd
ryesil <85940052+ryesil@users.noreply.github.com>
2021-08-08 12:44:58 -0500

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,6 +1,7 @@
 public class TestClass {
     public static void main(String[] args){
         System.out.println("This is local repo");
+        System.out.println("This is remote repo");
         
     }
 }
